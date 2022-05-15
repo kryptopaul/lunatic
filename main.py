1e75e13971b07595de2a0687b876b983a24e7865
xisux <87016182+xisux@users.noreply.github.com>
2022-05-15 22:28:02 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import time
+
+def main():
+  while True:
+    #Fetches the LUNA price from Coingecko
+    r_luna = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=terra-luna&vs_currencies=usd', headers={'accept': 'application/json'})
+    luna_price = float(r_luna.json()['terra-luna']['usd'])
+    
+    #Loads the configuration
+    config = json.load(open('config.json'))
+    luna_balance = config['luna_balance']
+    discord_webhook = config['discord_webhook']
+    refresh_rate = config['refresh_rate']
+
+    message = {
+      "content": None,
+      "embeds": [
+        {
+          "title": "LUNA Prices",
+          "description": f"Current LUNA price: {luna_price} USD",
+          "color": 4897036,
+          "fields": [
+            {
+              "name": "Your LUNA holdings",
+              "value": f"You own {luna_balance} LUNA worth {round(luna_price * luna_balance, 2)} USD"
+            }
+          ],
+          "thumbnail": {
+            "url": "https://cryptologos.cc/logos/terra-luna-luna-logo.png"
+          },
+        }
+      ],
+      "attachments": []
+    }
+
+    requests.post(discord_webhook, json=message)
+    print("Update sent!")
+    time.sleep(refresh_rate)
+
+if __name__ == "__main__":
+  main()
